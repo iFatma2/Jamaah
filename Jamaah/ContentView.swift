fe0825c75effaa994278e3aa3a11da17f04ad641
iFatma2 <fatma.m.q@outlook.sa>
2024-09-29 17:25:53 +0300

    Initial Commit

---
+++ Jamaah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jamaah
+//
+//  Created by Fatimah Alqarni on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jamaah/JamaahApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamaahApp.swift
+//  Jamaah
+//
+//  Created by Fatimah Alqarni on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct JamaahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
